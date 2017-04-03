35ef5b6d38dcd4764575b6c1fc97667ef9425696
Kyle Weems <weemsquirrel@gmail.com>
2017-04-03 12:36:27 -0700

    Added endpoint for datatype get API.

--- src/BulkEdit/bulkEdit.api.js
+++ src/BulkEdit/bulkEdit.api.js
@@ -1,9 +1,23 @@
 angular.module("umbraco.resources").factory("bulkEditApi", function($http) {
     return {
         getCsvExport: function(nodeId, doctypeAlias) {
-            return $http.get("/Umbraco/backoffice/ORCCsv/CsvExport/GetPublishedContent?format=Csv&contentTypeAlias=" + doctypeAlias + "&rootId=" + nodeId);         },
+            return $http.get(
+                "/Umbraco/backoffice/ORCCsv/CsvExport/GetPublishedContent?format=Csv&contentTypeAlias=" +
+                    doctypeAlias +
+                    "&rootId=" +
+                    nodeId
+            );
+        },
+        getDataTypeById: function(id) {
+            return $http.get("backoffice/ORCCsv/DataType/GetById?id=" + id);
+        },
         getMatchingContent: function(nodeId, doctypeAlias) {
-            return $http.get("/Umbraco/backoffice/ORCCsv/CsvExport/GetPublishedContent?format=json&contentTypeAlias=" + doctypeAlias + "&rootId=" + nodeId); 
+            return $http.get(
+                "/Umbraco/backoffice/ORCCsv/CsvExport/GetPublishedContent?format=json&contentTypeAlias=" +
+                    doctypeAlias +
+                    "&rootId=" +
+                    nodeId
+            );
         }
     };
 });
