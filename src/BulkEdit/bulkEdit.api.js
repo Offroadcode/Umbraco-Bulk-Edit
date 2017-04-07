9a2918499651a3e1483a52a8a9f6584facc857c8
Kyle Weems <weemsquirrel@gmail.com>
2017-04-07 13:12:11 -0700

    JS-side API for get/post to saved search API endpoints https://github.com/Offroadcode/Umbraco-Bulk-Edit/issues/10

--- src/BulkEdit/bulkEdit.api.js
+++ src/BulkEdit/bulkEdit.api.js
@@ -1,5 +1,8 @@
 angular.module("umbraco.resources").factory("bulkEditApi", function($http) {
     return {
+        getAllSavedSearches: function() {
+            return $http.get("/Umbraco/backoffice/ORCCsv/Database/getAllSavedSearches");
+        },
         getCsvExport: function(nodeId, doctypeAlias) {
             return $http.get(
                 "/Umbraco/backoffice/ORCCsv/CsvExport/GetPublishedContent?format=Csv&contentTypeAlias=" +
@@ -19,7 +22,17 @@ angular.module("umbraco.resources").factory("bulkEditApi", function($http) {
                     nodeId
             );
         },
-        SavePropertyForNode: function(nodeId, propertyName, propertyValue) {
+        getSavedSearchByGuid: function(guid) {
+            return $http.get('/Umbraco/backoffice/ORCCsv/Database/GetSavedSearchById/?guid=' + guid);
+        },
+        postSavedSearch: function(name, rootId, alias) {
+            var data = {
+                name: name,
+                options: JSON.stringify({rootId: rootId, alias: alias})
+            };
+            return $http.post('/Umbraco/backoffice/ORCCsv/Database/PostSavedSearch', data);
+        },
+        savePropertyForNode: function(nodeId, propertyName, propertyValue) {
             var data = {
                 nodeId: nodeId,
                 propertyName: propertyName,
@@ -27,7 +40,7 @@ angular.module("umbraco.resources").factory("bulkEditApi", function($http) {
             };
             return $http.get('/umbraco/backoffice/ORCCsv/Content/SavePropertyForNode/?nodeId=' + nodeId + '&propertyName=' + propertyName + '&propertyValue=' + propertyValue);
         },
-        SaveNodes: function(nodes) {
+        saveNodes: function(nodes) {
             var data = nodes;
             return $http.post('/umbraco/backoffice/ORCCsv/Content/SaveNodes', data);
         }
