9ffa89c9c806fc822f1bb0377c8c0aac9f5276bf
Kyle Weems <weemsquirrel@gmail.com>
2017-04-07 13:11:37 -0700

    Migration to install saved search DB. API to get/post to custom DB. https://github.com/Offroadcode/Umbraco-Bulk-Edit/issues/10

---
+++ src/BulkEdit/Umbraco/UmbracoBulkEdit/UmbracoBulkEdit/Controllers/DatabaseController.cs
@@ -0,0 +1,84 @@
+﻿namespace Orc.CsvExport.Controllers
+{
+    using Umbraco.Core;
+    using Umbraco.Core.Persistence;
+    using Umbraco.Web.Mvc;
+    using Umbraco.Web.WebApi;
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+    using System.Collections.Generic;
+    using ORCCsv.Models;
+
+    [PluginController("ORCCsv")]
+    public class DatabaseController : UmbracoAuthorizedApiController
+    {
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage GetAllSavedSearches()
+        {
+            var db = ApplicationContext.Current.DatabaseContext.Database;
+            var results = db.Fetch<BulkSavedSearch>("SELECT * from BulkSavedSearch");
+            var response = buildSerializedResponse(new
+            { 
+                results = results
+            }); 
+            return response;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet] 
+        public HttpResponseMessage GetSavedSearchByGuid(string guid)
+        {
+            var db = ApplicationContext.Current.DatabaseContext.Database;
+            var results = db.FirstOrDefault<BulkSavedSearch>("SELECT * from BulkSavedSearch WHERE Guid=@0", guid);
+            var response = buildSerializedResponse(new
+            {
+                results = results
+            });
+            return response;
+        }
+
+        private HttpResponseMessage buildSerializedResponse(object objectToReturn)
+        {
+            var serialized = Newtonsoft.Json.JsonConvert.SerializeObject(objectToReturn);
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(serialized)
+            };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return response;
+        }
+
+        private string saveSearch(string name, string options)
+        {
+            var db = ApplicationContext.Current.DatabaseContext.Database;
+            Guid g = Guid.NewGuid();
+            var guid = g.ToString();
+            var bulkSavedSearch = new BulkSavedSearch
+            {
+                Guid = guid,
+                Name = name,
+                Options = options
+            };
+            db.Insert(bulkSavedSearch);
+            return guid;
+        }
+
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage PostSavedSearch(BulkSavedSearch search)
+        {
+            var guid = saveSearch(search.Name, search.Options);
+            search.Guid = guid;
+            var response = buildSerializedResponse(new
+            {
+                search = search,
+                submitted = true
+            });
+            return response;
+        }
+    }
+}
---
+++ src/BulkEdit/Umbraco/UmbracoBulkEdit/UmbracoBulkEdit/Installer/Migrations.cs
@@ -0,0 +1,36 @@
+﻿using ORCCsv.Models;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.Migrations;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+// https://cultiv.nl/blog/using-umbraco-migrations-to-deploy-changes/
+namespace ORCCsv.Migrations
+{
+    [Migration("1.0.0", 1, "BulkSavedSearch")]
+    public class CreateBulkSavedSearchTable : MigrationBase
+    {
+        private readonly UmbracoDatabase _database = ApplicationContext.Current.DatabaseContext.Database;
+        private readonly DatabaseSchemaHelper _schemaHelper;
+
+        public CreateBulkSavedSearchTable(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+          : base(sqlSyntax, logger)
+        {
+            _schemaHelper = new DatabaseSchemaHelper(_database, logger, sqlSyntax);
+        }
+
+        public override void Up()
+        {
+            _schemaHelper.CreateTable<BulkSavedSearch>(false);
+
+            // Remember you can execute ANY code here and in Down().. 
+            // Anything you can think of, go nuts (not really!)
+        }
+
+        public override void Down()
+        {
+            _schemaHelper.DropTable<BulkSavedSearch>();
+        }
+    }
+}
